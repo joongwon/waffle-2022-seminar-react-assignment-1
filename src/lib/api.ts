cb120b7025b09ab32d49ce1dc2d34aad2150b95a
joongwon <winterdawnlight@snu.ac.kr>
2022-11-19 22:19:41 +0900

    refine api logic

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -79,37 +79,25 @@ export const apiDeleteReview = (id: number, token: string) =>
 export function useApiData<T>(
   fetch: ((cancel: CancelToken) => Promise<AxiosResponse<T>>) | null
 ) {
-  const [data, setData] = useState<T | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<{ payload: unknown } | null>(null);
-  const cancelRef = useRef<CancelTokenSource | null>(null);
-  const cancel = useCallback(() => {
-    cancelRef.current?.cancel();
-  }, []);
+  const [{ data, error, loading }, setResult] = useState<{
+    data?: T;
+    error?: { payload?: unknown };
+    loading: boolean;
+  }>({ loading: false });
   useEffect(() => {
-    (async () => {
-      if (!fetch) return null;
-      setLoading(true);
-      cancel();
-      const source = axios.CancelToken.source();
-      cancelRef.current = source;
-
-      try {
-        const res = await fetch(source.token);
-        setData(res.data);
-        return res;
-      } catch (reason) {
-        if (axios.isCancel(reason)) return;
-        setData(null);
-        setError({ payload: reason });
-        throw reason;
-      } finally {
-        cancelRef.current = null;
-        setLoading(false);
-      }
-    })();
-    return () => cancel();
-  }, [cancel, fetch]);
+    if (!fetch) return;
+    const source = axios.CancelToken.source();
+    setResult((prev) => ({ ...prev, loading: true }));
+    fetch(source.token)
+      .then((res) => {
+        setResult({ data: res.data, loading: false });
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setResult({ error: { payload: err }, loading: false });
+      });
+    return () => source.cancel();
+  }, [fetch]);
   return { data, loading, error };
 }
 
@@ -158,67 +146,80 @@ export const useApiOwnerList = (name: string | null) =>
     [name]
   );
 
-export function useApiReviewInfScroll(_menu: number, count: number) {
-  const [data, setData] = useState<Review[]>([]);
-  const [from, setFrom] = useState<string | undefined>();
-  const [menu, setMenu] = useState(_menu);
-  const [end, setEnd] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const cancelRef = useRef<CancelTokenSource | null>(null);
-  const cancel = useCallback(() => {
-    cancelRef.current?.cancel();
-  }, []);
-  const fetch = useCallback(
-    async (menu: number, count: number, from?: string) => {
-      setLoading(true);
-      cancel();
-      const source = axios.CancelToken.source();
-      cancelRef.current = source;
-      try {
-        const res = await axios.get<{ data: Review[]; next: string }>(
-          url("/reviews/", {
-            menu: menu.toString(),
-            count: count.toString(),
-            ...(from && { from }),
-          }),
-          { cancelToken: source.token }
-        );
-        return res.data;
-      } finally {
-        cancelRef.current = null;
-        setLoading(false);
-      }
-    },
-    [cancel]
-  );
-  const refresh = useCallback(
-    () =>
-      fetch(menu, count)
-        .then((resData) => {
-          setData(resData.data);
-          setFrom(resData.next);
-          setEnd(resData.data.length === 0);
-        })
-        .catch((err) => {
-          if (!axios.isCancel(err)) throw err;
+const useApiReviewList = (menu: number, count: number) =>
+  useCallback(
+    (from: string | undefined, cancelToken: CancelToken) =>
+      axios.get<{ data: Review[]; next: string }>(
+        url("/reviews/", {
+          menu: menu.toString(),
+          count: count.toString(),
+          ...(from && { from }),
         }),
-    [count, fetch, menu]
+        { cancelToken }
+      ),
+    [count, menu]
   );
-  const next = useCallback(async () => {
-    if (end || loading) return;
-    const resData = await fetch(menu, count, from);
-    if (resData.data.length === 0) {
-      setEnd(true);
-      return;
-    }
-    setData([...data, ...resData.data]);
-    setFrom(resData.next);
-  }, [count, data, end, fetch, from, loading, menu]);
-  useEffect(() => {
-    if (menu !== _menu) {
-      setData([]);
-      setMenu(_menu);
-    }
-  }, [_menu, menu]);
-  return { refresh, next, data, cancel, loading };
+
+export function useApiReviewInfScroll(menu: number, count: number) {
+  const [{ data, from, isEnd, error, loading }, setResult] = useState<{
+    data: Review[];
+    from?: string;
+    isEnd: boolean;
+    error?: { payload: unknown };
+    loading: boolean;
+  }>({ data: [], loading: false, isEnd: false });
+  const cancelRef = useRef<CancelTokenSource | null>(null);
+  const fetch = useApiReviewList(menu, count);
+  const next = useCallback(() => {
+    if (loading || isEnd) return;
+    const source = (cancelRef.current = axios.CancelToken.source());
+    setResult((prev) => ({ ...prev, loading: true }));
+    fetch(from, source.token)
+      .then((res) => {
+        setResult((prev) => ({
+          data: [...prev.data, ...res.data.data],
+          from: res.data.next,
+          isEnd: res.data.data.length === 0,
+          loading: false,
+        }));
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setResult((prev) => ({
+          ...prev,
+          error: { payload: err },
+          loading: false,
+        }));
+      })
+      .catch(() => {
+        cancelRef.current = null;
+      });
+  }, [loading, isEnd, fetch, from]);
+  const cancel = useCallback(() => cancelRef.current?.cancel(), []);
+  const refresh = useCallback(() => {
+    cancelRef.current?.cancel();
+    const source = (cancelRef.current = axios.CancelToken.source());
+    setResult((prev) => ({ ...prev, loading: true }));
+    fetch(undefined, source.token)
+      .then((res) => {
+        setResult({
+          data: [...res.data.data],
+          from: res.data.next,
+          isEnd: res.data.data.length === 0,
+          loading: false,
+        });
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setResult((prev) => ({
+          ...prev,
+          error: { payload: err },
+          loading: false,
+        }));
+      })
+      .catch(() => {
+        cancelRef.current = null;
+      });
+  }, [fetch]);
+  return { refresh, next, data, cancel, loading, error };
 }
